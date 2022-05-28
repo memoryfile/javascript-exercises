70011e49fd97f24df50a020a6d9ad1760073861f
memoryfile <yuiotp999@protonmail.com>
2022-05-28 19:27:09 -0400

    Create array

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,16 @@
-const reverseString = function() {
+const reverseString = function(word) {
+   
+    // Create empty array
+    const reverseArray = [];
+    const length = word.length - 1;
 
+    // Loop from end
+    for (let i = length; i >= 0; i--) {
+        reverseArray.push(word[i]);
+
+    // Join array elements
+    return reverseArray.join('');
+    }
 };
 
 // Do not edit below this line
