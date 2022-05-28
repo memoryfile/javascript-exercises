b6faa08e5f00a09840609acd577fe2aeb1428f61
memoryfile <yuiotp999@protonmail.com>
2022-05-28 19:09:37 -0400

    Write repeatString function

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,8 +1,10 @@
-const string = 'hey';
-
-const repeatString = function () {
-    repeatString(string, 3) // returns 'heyheyhey'
-
+const repeatString = function(word, times) { // define `word` and `times` inside the function itself without needing a separate `let`
+        if (times < 0) return 'ERROR'; // if the number is less than 0, return an error
+        let string = ''; // string = a blank string
+        for (let i = 0; i < times; i++) { // basic for loop
+            string += word; // concatenates the heys
+    }
+    return string; // returns everything after concatenation
 };
 
 // Do not edit below this line
