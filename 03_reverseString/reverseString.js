3ec1fa85231dccf8124758d525f087976b2fe7a5
memoryfile <yuiotp999@protonmail.com>
2022-05-28 19:31:00 -0400

    Fix formatting

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,17 +1,15 @@
-const reverseString = function(word) {
-   
-    // Create empty array
+const reverseString = function (word) { // Create empty array
     const reverseArray = [];
     const length = word.length - 1;
 
     // Loop from end
     for (let i = length; i >= 0; i--) {
         reverseArray.push(word[i]);
-
-    // Join array elements
-    return reverseArray.join('');
     }
-};
+
+        // Join array elements
+        return reverseArray.join('');
+    };
 
 // Do not edit below this line
 module.exports = reverseString;
