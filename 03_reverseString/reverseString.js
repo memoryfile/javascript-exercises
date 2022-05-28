d8a32bfa1751b5fc8ebd09cf70bb0d38af52a76e
memoryfile <yuiotp999@protonmail.com>
2022-05-28 19:43:23 -0400

    Add too many comments

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,14 +1,14 @@
-const reverseString = function (word) { // Create empty array
-    const reverseArray = [];
-    const length = word.length - 1;
+const reverseString = function (word) { 
+    const reverseArray = []; // Create empty array
+    const length = word.length - 1; // Length is word length minus one
 
     // Loop from end
-    for (let i = length; i >= 0; i--) {
-        reverseArray.push(word[i]);
+    for (let i = length; i >= 0; i--) { // i = word length minus one, so subtract one length every for loop
+        reverseArray.push(word[i]); // with each for loop, push a letter as a word into the array
     }
 
         // Join array elements
-        return reverseArray.join('');
+        return reverseArray.join(''); // join the letters together
     };
 
 // Do not edit below this line
