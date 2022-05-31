c1805a6049f63559c17e4adc349fd1bd43cbcde8
memoryfile <yuiotp999@protonmail.com>
2022-05-31 15:22:11 -0400

    Define remove function

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,8 +1,23 @@
-const myArray = [1, 2, 3, 4, 5, 6, 7];
+const removeFromArray = function (object) {
+    const myArray = [
+        1,
+        2,
+        3,
+        4,
+        5,
+        6,
+        7
+    ];
+    function remove(myArray, removeFromArray) {
+        for (let i = length; i >= 0; i--) {
+            return removeFromArray.splice(myArray.indexOf(object[i]));
+        }
 
-const removeFromArray = function() {
-    (myArray.splice(removeFromArray));
-};
+        // Join array elements
+        return myArray.push('');
+
+    };
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
