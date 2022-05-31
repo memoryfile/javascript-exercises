ad260ef21caca46db13b51e060aa4bda0013919d
memoryfile <yuiotp999@protonmail.com>
2022-05-31 14:47:07 -0400

    Remove test skips

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,7 +1,7 @@
 const myArray = [1, 2, 3, 4, 5, 6, 7];
 
 const removeFromArray = function() {
-    myArray.splice(removeFromArray);
+    (myArray.splice(removeFromArray));
 };
 
 // Do not edit below this line
