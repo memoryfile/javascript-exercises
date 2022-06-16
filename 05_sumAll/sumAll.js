cfa0b72565ff910299b449fc502b4db6d62e26a8
memoryfile <yuiotp999@protonmail.com>
2022-06-16 12:37:28 -0400

    Import pseudocode

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,11 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+    var finalSum = sumAll();
+    let process = min + max;
+    for (let summing = 0; summing < process; summing++) {
+        return finalSum();
+    }
+}
 
-};
 
 // Do not edit below this line
 module.exports = sumAll;
