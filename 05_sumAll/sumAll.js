7396f4059a43c7515f1d18afa1171236940ba41a
memoryfile <yuiotp999@protonmail.com>
2022-06-16 12:53:17 -0400

    Remove test skips, check for negative numbers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,11 +1,16 @@
-const sumAll = function(min, max) {
-    var finalSum = sumAll();
-    let process = min + max;
-    for (let summing = 0; summing < process; summing++) {
-        return finalSum();
+const sumAll = function (number1, number2) {
+    // const error = 'ERROR';
+    if (number1 || number2 < 0) {
+        var finalSum = sumAll();
+        let process = number1 + number2;
+        for (let summing = 0; summing < process; summing++) {
+            return finalSum();
+        }
+    }
+    if (!number1 || !number2 < 0) {
+        throw 'ERROR';
     }
 }
-
 
 // Do not edit below this line
 module.exports = sumAll;
