cbedae6578c4335680b793f7bb44bab8db463615
memoryfile <yuiotp999@protonmail.com>
2022-06-17 12:08:28 -0400

    Change error handling, change loop logic, change ifs

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,14 +1,24 @@
 const sumAll = function (number1, number2) {
     // const error = 'ERROR';
+    if (!number1 || !number2 < 0 || number1 == undefined || number2 == undefined) {
+        return undefined;
+    }
     if (number1 || number2 < 0) {
         var finalSum = sumAll();
-        let process = number1 + number2;
-        for (let summing = 0; summing < process; summing++) {
+        let compareInputs = number1 + number2;
+        if (number1 > number2) {
+            return number1 == highestInput;
+        }
+        if (number2 > number1) {
+            return number2 == highestInput;
+        }
+
+        for (let count = 0; count < highestInput; count++) {
+            number1 + count;
             return finalSum();
         }
-    }
-    if (!number1 || !number2 < 0) {
-        throw 'ERROR';
+
+        // let finalSum() = compareInputs + count;
     }
 }
 
