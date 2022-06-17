fa0fe2af01fbbd6aa319e09929c716eab60a2277
memoryfile <yuiotp999@protonmail.com>
2022-06-17 15:49:34 -0400

    Remove GreaterThan functions

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,6 @@
-const number1IsGreaterThanNumber2 = new(Boolean);
+// const number1IsGreaterThanNumber2 = new(Boolean);
 
-const number2IsGreaterThanNumber1 = new(Boolean);
+// const number2IsGreaterThanNumber1 = new(Boolean);
 
 const sumAll = function (number1, number2) { // const error = 'ERROR';
     if (! number1 || ! number2 < 0 || number1 == undefined || number2 == undefined) {
@@ -11,13 +11,13 @@ const sumAll = function (number1, number2) { // const error = 'ERROR';
     }
     if (number1 && number2 >= 0) {
 
-        if (number1 > number2) {
-            return number1IsGreaterThanNumber2();
-        }
+        // if (number1 > number2) {
+        //     return number1IsGreaterThanNumber2();
+        // }
 
-        if (number2 > number1) {
-            return number2IsGreaterThanNumber1();
-        }
+        // if (number2 > number1) {
+        //     return number2IsGreaterThanNumber1();
+        // }
 
     function addInputs(number1, number2) {
         return number1 + number2;
@@ -35,13 +35,13 @@ const sumAll = function (number1, number2) { // const error = 'ERROR';
 
         // let highestInput = compareInputs.
 
-        if (number1IsGreaterThanNumber2()) {
+        if (number1 > number2) {
             for (let count = 0; count < number1; count++) {
                 return sumAll(addInputs + count);
             }
         }
 
-        if (number2IsGreaterThanNumber1()) {
+        if (number2 > number1) {
             for (let count = 0; count < number2; count++) {
                 return sumAll(addInputs + count);
             }
