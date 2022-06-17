4fc891b7a97950bd538a4d453b554c758fef6ec8
memoryfile <yuiotp999@protonmail.com>
2022-06-17 15:46:26 -0400

    Change greater number logic, add booleans

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,3 +1,7 @@
+const number1IsGreaterThanNumber2 = new(Boolean);
+
+const number2IsGreaterThanNumber1 = new(Boolean);
+
 const sumAll = function (number1, number2) { // const error = 'ERROR';
     if (! number1 || ! number2 < 0 || number1 == undefined || number2 == undefined) {
         return String('ERROR');
@@ -5,7 +9,7 @@ const sumAll = function (number1, number2) { // const error = 'ERROR';
     if (isNaN(number1 || number2)) {
         return String('ERROR');
     }
-    if (number1 || number2 >= 0) {
+    if (number1 && number2 >= 0) {
 
         if (number1 > number2) {
             return number1IsGreaterThanNumber2();
@@ -15,27 +19,31 @@ const sumAll = function (number1, number2) { // const error = 'ERROR';
             return number2IsGreaterThanNumber1();
         }
 
-        let compareInputs = function (number1IsGreaterThanNumber2, number2IsGreaterThanNumber1) {
-            if (number1IsGreaterThanNumber2 == true) {
-                return highestInput = number1;
-            } else if (number2IsGreaterThanNumber1 == true) {
-                return highestInput = number2;
-            }
-        }
+    function addInputs(number1, number2) {
+        return number1 + number2;
+    }
+
+        // let compareInputs = function (number1IsGreaterThanNumber2, number2IsGreaterThanNumber1) {
+        //     if (number1IsGreaterThanNumber2 == true) {
+        //         // return highestInput = number1;
+        //         return highestInput;
+        //     } else if (number2IsGreaterThanNumber1 == true) {
+        //         // return highestInput = number2;
+        //         return highestInput;
+        //     }
+        // }
 
         // let highestInput = compareInputs.
 
-        function number1IsGreaterThanNumber2() {
-            for (let count = 0; count < highestInput; count++) {
-                number1 + count + compareInputs;
-                return sumAll();
+        if (number1IsGreaterThanNumber2()) {
+            for (let count = 0; count < number1; count++) {
+                return sumAll(addInputs + count);
             }
         }
 
-        function number2IsGreaterThanNumber1() {
-            for (let count = 0; count < highestInput; count++) {
-                number2 + count + compareInputs;
-                return sumAll();
+        if (number2IsGreaterThanNumber1()) {
+            for (let count = 0; count < number2; count++) {
+                return sumAll(addInputs + count);
             }
         }
 
