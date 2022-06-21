0242b460f3c7ae36b525dfcd138a1012abe91bee
memoryfile <yuiotp999@protonmail.com>
2022-06-21 14:51:48 -0400

    Study solution and apply it

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,44 +1,33 @@
-// const number1IsGreaterThanNumber2 = new(Boolean);
+const sumAll = function (min, max) {
+    if (!Number.isInteger(min) || !Number.isInteger(max)) 
+        return "ERROR";
+    
 
-// const number2IsGreaterThanNumber1 = new(Boolean);
 
-const sumAll = function (number1, number2) {
-    if (! number1 || ! number2 < 0 || number1 === undefined || number2 === undefined) {
-        return String('ERROR');
+    if (min < 0 || max < 0) 
+        return "ERROR";
+    
+
+
+    if (sumAll(Array.isArray)) 
+        return "ERROR";
+    
+
+
+    if (min > max) {
+        const temp = min;
+        min = max;
+        max = temp;
     }
-    if (isNaN(number1 || number2)) {
-        return String('ERROR');
+    let sum = 0;
+
+    for (let i = min; i < max + 1; i++) {
+        sum += i;
     }
-    if (sumAll(Array.isArray)) {
-        return String('ERROR');
-    }
+    return sum;
+};
 
-    let finalSum = 0;
 
-    // if (number1 >= 0 && number2 >= 0) {
-    //     if (! number1 == NaN || ! number2 == NaN) {
+// Do not edit below this line
 
-    // function addInputs(number1, number2) {
-    //     if (number1 > number2) {
-    //         return number1 + number2;
-    //     }
-    //     if (number2 > number1) {
-    //         return number2 + number1;
-    //     }
-    // }
-
-        if (number1 > number2) {
-            for (let count = 0; count <= number1; count++) {
-                return finalSum + count && console.log(finalSum);
-            }
-        }
-
-        if (number2 > number1) {
-            for (let count = 0; count <= number2; count++) {
-                return finalSum + count && console.log(finalSum);
-            }
-        }
-    };
-
-    // Do not edit below this line
-    module.exports = sumAll;
+module.exports = sumAll;
