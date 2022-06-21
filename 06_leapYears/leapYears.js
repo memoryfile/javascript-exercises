83a8d48c8cba6d994a51871e90e036527c66a28f
memoryfile <yuiotp999@protonmail.com>
2022-06-21 15:54:08 -0400

    Check divisions of 400 and 100

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,9 +1,11 @@
-const leapYears = function(year) {
-if (year % 4 == 0) {
-    return true; 
-} else {
-    return false;
-}
+const leapYears = function (year) {
+    if (year % 100 == 0 && year % 400 !== 0) {
+        return false;
+    } else if (year % 4 == 0) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
