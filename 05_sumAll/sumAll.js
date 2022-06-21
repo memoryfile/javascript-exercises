5036f0235e570187494d5c5a84f6f8c4c2a0f2bf
memoryfile <yuiotp999@protonmail.com>
2022-06-21 13:34:47 -0400

    Edit for loop returns

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -13,32 +13,32 @@ const sumAll = function (number1, number2) {
         return String('ERROR');
     }
 
+    let finalSum = 0;
+
     // if (number1 >= 0 && number2 >= 0) {
     //     if (! number1 == NaN || ! number2 == NaN) {
 
-    function addInputs(number1, number2) {
-        if (number1 > number2) {
-            return number1 + number2;
-        }
-        if (number2 > number1) {
-            return number2 + number1;
-        }
-    }
+    // function addInputs(number1, number2) {
+    //     if (number1 > number2) {
+    //         return number1 + number2;
+    //     }
+    //     if (number2 > number1) {
+    //         return number2 + number1;
+    //     }
+    // }
 
         if (number1 > number2) {
-            for (let count = 0; count < number1; count++) {
-                console.log;
-                return sumAll(addInputs + count);
+            for (let count = 0; count <= number1; count++) {
+                return finalSum + count && console.log(finalSum);
             }
         }
 
         if (number2 > number1) {
-            for (let count = 0; count < number2; count++) {
-                console.log;
-                return sumAll(addInputs + count);
+            for (let count = 0; count <= number2; count++) {
+                return finalSum + count && console.log(finalSum);
             }
         }
-    }
+    };
 
     // Do not edit below this line
     module.exports = sumAll;
