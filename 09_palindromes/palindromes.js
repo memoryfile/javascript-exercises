278f7b53d6d2a50a40058d0fbbb707b514bb4596
memoryfile-desktop <yuiotp999@protonmail.com>
2022-06-23 15:21:14 -0400

    Create lowerReplaceString and reverseString

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  let unwanted = /[^A-Za-z0–9]/g; // Best way to remove punctuation etc.
+  const lowerReplaceString = string.toLowerCase().replace(unwanted, ""); // Lowercase the string and remove unwanted aspects of it
+  const reverseString = lowerReplaceString.split("").reverse().join(""); // Split string, reverse it, and join it together again
+  return reverseString === lowerReplaceString; // Confirm the aforementioned
 };
 
 // Do not edit below this line
