97a1d81d05ea09d02716688a6c40f60cf8580304
memoryfile-desktop <yuiotp999@protonmail.com>
2022-06-23 14:26:01 -0400

    Study solution

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -6,22 +6,29 @@ const subtract = function (min, max) {
   return min - max;
 };
 
-const sum = function (input) {
-  // const array = [input];
-
-  for (let i = 0; i < input; i++) {
-    sum += input[i];
-  }
+const sum = function (array) {
+  return array.length
+  ? array.reduce((accumulator, nextItem) => accumulator + nextItem) //  reduce multiplies each value by the following item, and accumulator is the net result of a function
+  : 0;
 };
 
-const multiply = function (input) {};
+const multiply = function (array) {
+  return array.length
+  ? array.reduce((accumulator, nextItem) => accumulator * nextItem) //  reduce multiplies each value by the following item, and accumulator is the net result of a function
+  : 0;
+};
 
 const power = function (base, exponent) {
   return Math.pow(base, exponent);
 };
 
-const factorial = function (input) {
-  return input * (input - 1);
+const factorial = function (n) {
+  if (n === 0) return 1;
+  let input = 1;
+  for (let i = n; i > 0; i--) {
+  input *= i; 
+  }
+  return input; 
 };
 
 // Do not edit below this line
