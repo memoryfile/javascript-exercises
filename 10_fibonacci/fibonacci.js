fad1b37212fc754ba970b65beec5208a8b4407d5
memoryfile-desktop <yuiotp999@protonmail.com>
2022-06-23 15:57:49 -0400

    Make error message OOPS

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,7 +1,7 @@
 const fibonacci = function(array) {
     return array.length
     ? array.reduce((accumulator, nextItem) => accumulator + nextItem) //  reduce multiplies each value by the following item, and accumulator is the net result of a function
-    : 0;
+    : "OOPS";
 };
 
 // Do not edit below this line
