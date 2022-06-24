8025a3ddb3c2ec72d681f0b5bed8de8b62504ddb
memoryfile-desktop <yuiotp999@protonmail.com>
2022-06-24 14:38:32 -0400

    Study solution

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,10 +1,5 @@
-const getTheTitles = function (title, author) {
-  let array = [title, author];
-  //   //   array.push(title, author);
-  //   for (let i = title; i < array.length; i++) {
-  //     const titlesArray = [];
-  const sliceArray = array.slice(1);
-  return sliceArray[''];
+const getTheTitles = function (array) {
+  return array.map(book => book.title);
 };
 
 // Do not edit below this line
