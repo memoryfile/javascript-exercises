6d2ce82d673e09774fdcca1b46f44ec1ad6c4db4
memoryfile-desktop <yuiotp999@protonmail.com>
2022-06-24 12:42:01 -0400

    Create array

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(title, author) {
+let a = title;
+const titleArray = []
+// const authorArray = [];
+for (const arg of title) {
+    titleArray.push(a);
+}
+return titleArray;
 };
 
 // Do not edit below this line
