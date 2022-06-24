cb4b53a3a299a5b82b51a07c4bf56ab7049eb3c6
memoryfile-desktop <yuiotp999@protonmail.com>
2022-06-24 15:01:13 -0400

    Implement reduce method in ageofPeople const

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (array) {
+//   const array = ["Carly", "Ray", "Jane"];
+  const initialValue = 0;
+  const ageOfPeople = array.reduce(
+    (yearofDeath, yearofBirth) => yearofDeath - yearofBirth,
+    initialValue
+  );
+  return ageOfPeople(array);
 };
 
 // Do not edit below this line
