78d00b551b48af76ffb28cea8691f2abefd21b66
memoryfile-desktop <yuiotp999@protonmail.com>
2022-06-27 15:17:37 -0400

    Check for the age of people in the array

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,8 +1,24 @@
 const findTheOldest = function (array) {
-  const array = ["Carly", "Ray", "Jane"];
-  const initialValue = 0;
-  const age = yearofDeath - yearofBirth;
-  return ageOfPeople(array);
+  // const array = ["Carly", "Ray", "Jane"];
+  const age = yearOfDeath - yearOfBirth;
+  if (Carly.age > Ray.age && Jane.age) {
+    return carlyIsOldest();
+  }
+  if (Ray.age > Carly.age && Jane.age) {
+    return rayIsOldest();
+  }
+  if (Jane.age > Ray.age && Carly.age) {
+    return janeIsOldest();
+  }
+  if (janeIsOldest == true) {
+    return "Jane";
+  }
+  if (rayIsOldest == true) {
+    return "Ray";
+  }
+  if (CarlyIsOldest == true) {
+    return "Carly";
+  }
 };
 
 // Do not edit below this line
