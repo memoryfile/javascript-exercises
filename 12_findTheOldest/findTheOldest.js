4d51f2ae068b81635ae6e7c536362c5abfd54d31
memoryfile-desktop <yuiotp999@protonmail.com>
2022-06-27 15:49:50 -0400

    Bring back reduce, define age and findOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,24 +1,28 @@
 const findTheOldest = function (array) {
-  // const array = ["Carly", "Ray", "Jane"];
-  const age = yearOfDeath - yearOfBirth;
-  if (Carly.age > Ray.age && Jane.age) {
-    return carlyIsOldest();
-  }
-  if (Ray.age > Carly.age && Jane.age) {
-    return rayIsOldest();
-  }
-  if (Jane.age > Ray.age && Carly.age) {
-    return janeIsOldest();
-  }
-  if (janeIsOldest == true) {
-    return "Jane";
-  }
-  if (rayIsOldest == true) {
-    return "Ray";
-  }
-  if (CarlyIsOldest == true) {
-    return "Carly";
-  }
+  array.reduce((oldestAge, people) => {
+    const age = people.yearOfDeath - people.yearOfBirth;
+    const findOldest = Math.max(...array)
+    return findOldest;
+  }, 0);
+  
+  // if (Carly.age > Ray.age && Jane.age) {
+  //   return carlyIsOldest();
+  // }
+  // if (Ray.age > Carly.age && Jane.age) {
+  //   return rayIsOldest();
+  // }
+  // if (Jane.age > Ray.age && Carly.age) {
+  //   return janeIsOldest();
+  // }
+  // if (janeIsOldest == true) {
+  //   return "Jane";
+  // }
+  // if (rayIsOldest == true) {
+  //   return "Ray";
+  // }
+  // if (CarlyIsOldest == true) {
+  //   return "Carly";
+  // }
 };
 
 // Do not edit below this line
